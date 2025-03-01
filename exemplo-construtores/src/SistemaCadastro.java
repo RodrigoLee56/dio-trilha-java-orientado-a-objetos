691c1cf3d1774a73718088acb54fda16a8bdbadb
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2025-03-01 11:59:06 -0400

    feat: criar projeto exemplo-construtores

---
+++ exemplo-construtores/src/SistemaCadastro.java
@@ -0,0 +1,8 @@
+
+public class SistemaCadastro {
+	public static void main(String[] args) {
+		Pessoa marcos = new Pessoa("06724506716","MARCOS SILVA");
+		marcos.setEndereco("RUA DAS MARIAS");
+		System.out.println(marcos.getNome() + " - " + marcos.getCpf());
+	}
+}
