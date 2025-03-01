1d2f41ee3cbf43b4e50a4f09dc7c78b03b26efb6
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2025-03-01 00:22:18 -0400

    feat: criar e implementar as classes Escola e Aluno

---
+++ exemplo-getter-setter/src/br/org/escolajava/escola/Escola.java
@@ -0,0 +1,13 @@
+package br.org.escolajava.escola;
+
+import br.org.escolajava.aluno.Aluno;
+
+public class Escola {
+	public static void main(String[] args) {
+		Aluno felipe = new Aluno();
+		felipe.setNome("Felipe");
+		felipe.setIdade(8);
+
+		System.out.println("O aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " anos ");
+	}
+}
